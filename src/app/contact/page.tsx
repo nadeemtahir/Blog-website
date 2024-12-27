68914e5d5f7341d62c4fa9aec301e64d678ceba9
nadeemtahir <163245288+nadeemtahir@users.noreply.github.com>
2024-12-27 09:11:12 -0800

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
     <section className="text-gray-800 bg-neutral-200 body-font relative">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
-          <h1 className="sm:text-xl text-8xl font-bold title-font mb-4 text-gray-900">Contact Us</h1>
+          <h1 className="sm:text-sm text-8xl font-bold title-font mb-4 text-gray-900">Contact Us</h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-lg">
             Feel free to reach out for any inquiries, feedback, or support. Were here to help!
           </p>
