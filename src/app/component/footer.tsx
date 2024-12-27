0fd76d23288aa3f87ac71b876fbc66709ae4faf5
nadeemtahir <163245288+nadeemtahir@users.noreply.github.com>
2024-12-27 08:58:56 -0800

    Update footer.tsx

--- src/app/component/footer.tsx
+++ src/app/component/footer.tsx
@@ -5,9 +5,6 @@ function footer() {
     <footer className="text-gray-600 body-font bg-gray-900">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
     <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-300">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-      </svg>
       <span className="ml-3 text-xl">Manahil Nadeem</span>
     </a>
     <p className="text-sm text-gray-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
